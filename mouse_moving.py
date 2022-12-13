e14d135fbddf5fb5ba813cdd64fc2435b7f5238d
Cesar David <60083406+xesard@users.noreply.github.com>
2022-12-12 23:54:33 -0300

    Update mouse_moving.py

--- mouse_moving.py
+++ mouse_moving.py
@@ -7,3 +7,6 @@ while time.time() - start_time < 27 * 60:
     pyautogui.moveTo(pyautogui.position()[0] + 1, pyautogui.position()[1] + 1, duration=0.001)
     if pyautogui.keyDown("x"):
         break
+
+# Este script se ejecutará durante 27 minutos, moviendo el cursor en círculos pequeños. Si presionas la tecla "x" en cualquier momento, el script se cancelará.
+# Tenga en cuenta que este script requiere la biblioteca **`pyautogui`**. Si no la tienes instalada, puedes instalarla ejecutando **`pip install pyautogui`** en tu consola.
